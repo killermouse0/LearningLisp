316a49f83035cb0e0a12ea26845f7ce1c4b4c4c2
Stephane Kattoor <stephane.kattoor@sakana.fr>
2023-03-05 15:51:20 +0100

    aspect ratio correction + color coding + typos

--- mandelbrot.lisp
+++ mandelbrot.lisp
@@ -1,14 +1,43 @@
 (ql:quickload "zpng")
 (ql:quickload "bordeaux-threads")
 
-(defparameter *pic-width* 3840)
-(defparameter *pic-height* 2160)
-(defparameter *xmin* -2.0)
-(defparameter *xmax* +0.7)
-(defparameter *ymin* -1.5)
-(defparameter *ymax* +1.5)
+(defparameter *pic-width* 7680)
+(defparameter *pic-height* 4320)
+
+(defparameter *x-o* -0.7)
+(defparameter *y-o* 0.0)
+
+(defparameter *x-l* 2.0)
+(defparameter *xmin* (- *x-o* *x-l*))
+(defparameter *xmax* (+ *x-o* *x-l*))
+
+(defparameter *y-l* (/ (* *x-l* (/ *pic-height* 2)) (/ *pic-width* 2)))
+(defparameter *ymin* (- *y-o* *y-l*))
+(defparameter *ymax* (+ *y-o* *y-l*))
+
 (defparameter *rows-per-thread* 100)
-(defparameter *mandelbrot-max-iter* 255)
+(defparameter *mandelbrot-max-iter* 500)
+
+;; color coding
+
+(defun hsv-to-rgb (H S V)
+  (let* ((M (* 255 V))
+         (mm (* M (- 1.0 S)))
+         (z (* (- M mm)
+               (- 1
+                  (abs (- (mod (/ H 60) 2) 1))))))
+    (cond
+     ((< H 60) (values M (+ z mm) mm))
+     ((< H 120) (values (+ z mm) M mm))
+     ((< H 180) (values mm M (+ z mm)))
+     ((< H 240) (values mm (+ z mm) M))
+     ((< H 300) (values (+ z mm) m M))
+     (t (values M mm (+ z mm))))))
+
+(defun escape-to-color (n)
+  (let* ((h (* n (/ 360.0 *mandelbrot-max-iter*)))
+         (color (multiple-value-bind (r g b) (hsv-to-rgb h 1.0 1.0) (vector r g b))))
+    (map 'vector #'round color)))
 
 ;; coordinate mapping from picture coords to complex plane
 (defun j_to_x (j)
@@ -30,29 +59,29 @@
    ((> (abs z_n) 2) n)
    (t (mandelbrot (+ (* z_n z_n) c) c (+ n 1) n-max))))
 
-
 ;; image computation
 (defun compute-image-sector (row-min col-min row-max col-max image)
   (loop for i from row-min below row-max do
           (loop for j from col-min below col-max do
                   (let* ((c (pix_to_c i j))
-                         (final-iter (mandelbrot 0 c 0 *mandelbrot-max-iter*)))
-                    (setf (aref image i j 0)
-                      (cond
-                       ((eq final-iter *mandelbrot-max-iter*) 0) ;; ne converge pas 
-                       (t final-iter)) ;; converge
-                        ))))
+                         (final-iter (mandelbrot 0 c 0 *mandelbrot-max-iter*))
+                         (color (cond
+                                 ((eq final-iter *mandelbrot-max-iter*) (vector 0 0 0)) ;; ne converge pas 
+                                 (t (escape-to-color final-iter))))) ;; converge
+                    (setf (aref image i j 0) (aref color 0))
+                    (setf (aref image i j 1) (aref color 1))
+                    (setf (aref image i j 2) (aref color 2)))))
   (format nil "Done computing from ~A to ~A" row-min row-max))
 
 (defun compute-splits (mini maxi step acc)
   (cond ((> (+ mini step) maxi) (append acc (list (cons mini maxi))))
         (t (compute-splits (+ mini step) maxi step (append acc (list (cons mini (+ mini step))))))))
 
-(defun thread-ripper (threads)
+(defun thread-reaper (threads)
   (when threads
         (print (format nil "Waiting for thread ~A" (bt:thread-name (car threads))))
         (print (bt:join-thread (car threads)))
-        (thread-ripper (cdr threads))))
+        (thread-reaper (cdr threads))))
 
 (defun compute-image (row-max col-max image)
   (let ((threads (loop for s in (compute-splits 0 row-max *rows-per-thread* '()) collect
@@ -61,11 +90,11 @@
                                                  (row-end (cdr s)))
                                              (lambda ()
                                                (compute-image-sector row-start 0 row-end col-max image))) :name thread-name)))))
-    (thread-ripper threads)))
+    (thread-reaper threads)))
 
 (defun save_to_file (file)
   (let* ((png (make-instance 'zpng::png
-                :color-type :grayscale
+                :color-type :Truecolor
                 :width *pic-width*
                 :height *pic-height*))
          (image (zpng::data-array png)))
